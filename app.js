6854f07bb39dce6e3001492f2bdd14ce2b60f3e9
Dnyaneshwar Mogal <maulimogal@gmail.com>
2021-09-25 13:09:26 +0530

    alert box added

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-"use strict";
+const alertCloseIcon = document.querySelector("#crossIcon");
+const alertBoxDiv = document.querySelector("#alertBox");
+
+alertCloseIcon.addEventListener("click", (e) => {
+  alertBoxDiv.style.display = "none";
+});
